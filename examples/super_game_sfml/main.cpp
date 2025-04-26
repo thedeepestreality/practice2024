95921e7a0b0adb6c62fdb488541cf2d87c6539cb
Ruslan Sevostyanov <zetoster@gmail.com>
2025-04-26 11:55:36 +0300

    Added supergame with cmake

---
+++ examples/super_game_sfml/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+#include <SFML/Graphics.hpp>
+
+#include "GameState.h"
+#include "GameManager.h"
+
+void render_world(const GameState& state) {
+	system("cls");
+	std::cout << "Epoch " << state.epoch << '\n';
+	for (int row = 0; row < state.m_field.size(); ++row) {
+		for (int col = 0; col < state.m_field[0].size(); ++col) {
+			if (state.m_player_pos.row == row && state.m_player_pos.col == col)
+				std::cout << 'P';
+			else
+				switch (state.m_field[row][col]) {
+					case CellType::Empty:
+						std::cout << '.';
+						break;
+					case CellType::Wall:
+						std::cout << 'H';
+						break;
+				}
+		}
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+
+int main() {
+
+	auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+	/*
+	GameState init_state;
+	init_state.m_field = {
+		{CellType::Empty, CellType::Empty, CellType::Empty, CellType::Empty, CellType::Empty},
+		{CellType::Empty, CellType::Wall,  CellType::Empty, CellType::Wall,  CellType::Empty},
+		{CellType::Empty, CellType::Wall,  CellType::Empty, CellType::Wall,  CellType::Empty},
+		{CellType::Empty, CellType::Wall,  CellType::Empty, CellType::Wall,  CellType::Empty},
+		{CellType::Empty, CellType::Empty, CellType::Empty, CellType::Empty, CellType::Empty}
+	};
+	init_state.m_player_pos.row = 0;
+	init_state.m_player_pos.col = 0;
+
+	GameManager manager(init_state);
+
+	// Render inital field
+	render_world(manager.get_state());
+
+	for (int step = 0; step < 6; ++step) {
+		manager.move_player(Direction::Right);
+		// Render field
+		render_world(manager.get_state());
+	}
+
+	manager.move_player(Direction::Down);
+	// Render field
+	render_world(manager.get_state());
+
+	manager.move_player(Direction::Left);
+	// Render field
+	render_world(manager.get_state());
+
+	system("pause");
+*/
+	return 0;
+}
